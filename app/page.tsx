228aa153595cdfab5e78051a0ca90432623d0c05
setTimith <thomasabboud0@gmail.com>
2024-09-17 15:14:34 +0300

    Centering

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,7 @@ export default async function Home() {
           flexWrap: "wrap",
           gap: "20px",
           width: "800px",
+          justifyContent: "center",
         }}
       >
         {data.map((post, idx) => (
